71e31d3cbf85d41c9a8c8b00e3e5cf7789d0d9ba
hemakumar <hemakumarm@teamf1networks.com>
2016-03-19 12:19:56 +0530

    chore:validations and test
    Added validations and test-cases files

---
+++ validations/jsvalidations.js
@@ -0,0 +1,17 @@
+/*** This file contains code to validations in javascript ***/
+
+function Validations(){
+
+}
+
+/**
+*function requiredField decides filed need to be filled
+*param elementId string
+*param elementClass string
+*param element Object
+*returns true or false
+**/
+Validations.prototype.isObject = function(elementId, elementClass ,element){
+
+	return true;
+}
